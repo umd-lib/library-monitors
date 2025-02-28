77a803a01e75b9cd8c65c6c53d836955d4e7bff3
Jeremy M Gottwig <jeremy.m.gottwig@gmail.com>
2025-02-28 14:08:30 -0500

    LIBWEB-6328. Locations working for all libraries.
    https://umd-dit.atlassian.net/browse/LIBWEB-6328

--- monitors/blueprints/api/libapps.py
+++ monitors/blueprints/api/libapps.py
@@ -1,16 +1,14 @@
 import requests
 import monitors
-import time
 import datetime
 import pytz
 
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 
-from flask import Blueprint, request, redirect, session
-from flask.json import jsonify
+from flask import Blueprint, Response
 from cachetools import cached, TTLCache
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 
 libapps = Blueprint('libapps', __name__, url_prefix='/spaces/api')
 
@@ -24,7 +22,6 @@ def get_token():
     token = oauth.fetch_token(oauth_endpoint,
                                 client_id=monitors.libapps_client,
                                 client_secret=monitors.libapps_secret)
-    monitors.logger.error(token)
     if token['access_token'] is None:
         return None
     return token['access_token']
@@ -36,6 +33,7 @@ def authenticate():
         get_token.cache.clear()
 
     return token
+
 
 def make_api_request(url, params=None):
     token = authenticate()
@@ -52,10 +50,9 @@ def get_locations(location=None):
     locations_endpoint = monitors.libapps_api.url + '/space/locations'
     params = {'details': '0'}
     response = make_api_request(locations_endpoint, params)
-    monitors.logger.error(response.json())
 
     if response.json() is None:
-        return error_response('Response is empty')
+        return error_response('No Response', 400)
     if location is None:
         return response.json()
     
@@ -65,13 +62,12 @@ def get_locations(location=None):
             name = item['name'].lower()
             if location.lower() in name:
                 return item
-    return error_response
+    return error_response('No Response', 400)
+
 
 def get_location_details(lid):
     spaces_endpoint = monitors.libapps_api.url + '/space/items/' + str(lid)
-    # params = {'page_size': '100', 'availability': '2025-02-25'}
     params = {'page_size': '100', 'availability': 'next_only'}
-    monitors.logger.error(spaces_endpoint)
     response = make_api_request(spaces_endpoint, params)
 
     if response.content is None:
@@ -99,7 +95,6 @@ def app_spaces(location=None):
     else:
         for item in items:
             if item['lid'] is not None:
-                monitors.logger.error(item['lid'])
                 space = get_location_details(item['lid'])
                 if space is not None:
                     spaces.append(space)
@@ -111,47 +106,68 @@ def build_space_response(locations):
     current_group = None
     current_subsec = {} 
     current_avail = 0
+    current_total = 0
     current_next_avail = None
     overall_avail = 0
+    overall_total = 0
     for spaces in locations:
         for space in spaces:
             if 'groupId' in space and current_group != space['groupId']:
-                if (len(current_subsec) > 0):
+                # Consider refactoring. This does not cover last item
+                # in array.
+                if len(current_subsec) > 0:
+                    current_subsec['next_available'] = current_next_avail
                     response[current_group] = current_subsec
                     current_subsec = {}
+                    current_avail = 0
+                    current_total = 0
+                    current_next_avail = None
                 current_group = str(space['groupId'])
                 current_subsec['name'] = space['groupName']
                 # If they come out of order, use existing array
                 if current_group in response:
                     current_subsec = response[current_group]
                     current_avail = int(current_subsec['available'])
+                    current_total = int(current_subsec['total'])
+                    current_next_avail = current_subsec['next_available']
             if 'availability' in space and \
                 space['availability'][0] is not None and \
                 space['availability'][0]['to'] is not None:
                 to_date = space['availability'][0]['to']
-                if check_if_available(to_date):
+                from_date = space['availability'][0]['from']
+                current_total = current_total + 1
+                overall_total = overall_total + 1
+                if check_if_available(from_date, to_date):
                     overall_avail = overall_avail + 1
                     current_avail = current_avail + 1
                 current_subsec['available'] = str(current_avail)
+                current_subsec['total'] = str(current_total)
+                if current_next_avail is None:
+                    current_next_avail = from_date
+                elif compare_dates(current_next_avail, from_date):
+                    current_next_avail = from_date
+
+    # Add last item to array
+    if len(current_subsec) > 0:
+        current_subsec['next_available'] = current_next_avail
+        response[current_group] = current_subsec
+        
     response['overall_available'] = overall_avail
-    monitors.logger.error(response)
+    response['total'] = overall_total
     return response
 
 
-def check_if_available(to_date):
-    # 2025-02-26T06:30:00-05:00
-    # to_date = time.strptime(to_date, "%Y-%m-%dT%H:%M:%S%z")
-    to_date = datetime.fromisoformat('2025-02-26T06:30:00-05:00') 
-    monitors.logger.error(to_date)
+def check_if_available(from_date, to_date):
+    to_date = datetime.fromisoformat(to_date) 
+    from_date = datetime.fromisoformat(from_date)
     curr_time = datetime.now()
-    monitors.logger.error(tz.localize(curr_time))
-    return tz.localize(curr_time) < to_date
+    return from_date < tz.localize(curr_time) < to_date
 
 
 def compare_dates(date_1, date_2):
-    time_1 = time.strptime(date_1, "%Y-%m-%dT%H:%M:%S%z")
-    time_2 = time.strptime(date_2, "%Y-%m-%dT%H:%M:%S%z")
-    return time_1 < time_2
+    time_1 = datetime.fromisoformat(date_1)
+    time_2 = datetime.fromisoformat(date_2)
+    return time_1 > time_2
 
 
 @libapps.route('mckeldin')
@@ -163,10 +179,20 @@ def app_mckeldin_spaces():
 @libapps.route('stem')
 def app_stem_spaces():
     spaces = app_spaces('Stem')
-    # return spaces
-    resp = build_space_response(spaces)
-    return resp 
+    return build_space_response(spaces)
 
 
-def error_response(message='Response error'):
-    return message
+@libapps.route('mspal')
+def app_mspal_spaces():
+    spaces = app_spaces('Michelle Smith')
+    return build_space_response(spaces)
+
+
+@libapps.route('art')
+def app_art_spaces():
+    spaces = app_spaces('Art Library')
+    return build_space_response(spaces)
+
+
+def error_response(message='Response error', status=500):
+    return Response(message, status)
